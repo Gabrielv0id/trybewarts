37234b6414c1509fc1b8c4b1bc3b562659966bd7
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-09-14 16:03:11 -0300

    exercicio 3 feito

---
+++ script.js
@@ -0,0 +1,14 @@
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
+const btnEntrar = document.querySelector('#botao-entrar');
+
+function validarLogin(event) {
+  event.preventDefault();
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos');
+  }
+}
+
+btnEntrar.addEventListener('click', validarLogin);
